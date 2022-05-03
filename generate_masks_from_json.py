833931d50b6c06776bd181dfad60f73aabb0c30c
GAEUNYIM <rkdms4031@unist.ac.kr>
2022-05-03 15:05:28 +0900

    generate mask.png

--- generate_masks_from_json.py
+++ generate_masks_from_json.py
@@ -6,15 +6,68 @@ import cv2
 import matplotlib.pyplot as plt
 from pycocotools.coco import COCO
 
-path_source = "../../Media/v0/valid/"
+
+# Step / Set paths # TODO;
+path_source = "../../Media/v0/valid/" 
+path_source_images = path_source + "images"
 path_source_annotations = path_source + "annotations"
 path_source_masks = path_source + "masks"
 
-ann_file_name = 'coco_annotations.json'
-with open(path_source_annotations + '/' + ann_file_name, 'r') as jf:
+
+
+# Step / Set file name # TODO;
+ann_filename = 'coco_annotations.json'
+with open(path_source_annotations + '/' + ann_filename, 'r') as jf:
     json_data = json.load(jf)
     
 # Check : Print out all the keys in json_data (You can easily see the structure of json file)
 dict_keys = json_data.keys()
 print(dict_keys)
 
+# Get dictionaries
+js_dicts_images = json_data["images"]
+js_dicts_annotations = json_data["annotations"]
+
+
+
+
+# step / Returns lists of the file names
+list_images = os.listdir(path_source_images)
+
+
+
+# Step / Create inversed dictionaries with "image_id"
+ids_dict_with_file_names = {}
+
+for dict in js_dicts_images:
+    key = dict['file_name']
+
+    value = dict['id']
+    ids_dict_with_file_names[key] = value
+
+
+# Step / Load coco data
+coco_data = COCO(path_source_annotations + '/' + ann_filename)
+
+
+
+# Step / get annotation ids list of file image
+for file_name in list_images:
+    
+    if file_name == "191228_105517_252.jpg": # Test with this comlex figure
+
+        # Get image id from file_name
+        image_id = ids_dict_with_file_names[file_name]
+        
+        # Get annotation ids from file_name
+        ann_ids = coco_data.getAnnIds(imgIds=image_id)
+        
+        # Load annotation dictionaries from annotation ids
+        anns = coco_data.loadAnns(ann_ids)
+        for ann in anns:
+            mask = np.max(np.stack([coco_data.annToMask(ann) * ann["category_id"] * 100 for ann in anns]), axis=0)
+        
+        mask_name = file_name.replace('.jpg', '.png')
+
+        cv2.imwrite(mask_name, mask)
+        # cv2.imwrite(path_source_masks + "/" + mask_name, mask)
