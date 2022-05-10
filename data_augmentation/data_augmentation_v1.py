621cf2f1c7dd7b95749875dca1727d4465693a30
GAEUNYIM <rkdms4031@unist.ac.kr>
2022-05-10 10:29:33 +0900

    roate with mask

--- data_augmentation/data_augmentation_v1.py
+++ data_augmentation/data_augmentation_v1.py
@@ -2,10 +2,11 @@ import albumentations as A
 import cv2
 import os
 import json
-# import numpy as np
-# from collections import OrderedDict
-# import matplotlib.pyplot as plt
-# from PIL import Image
+from cv2 import BORDER_CONSTANT
+from pycocotools.coco import COCO
+import pycocotools.mask as pm
+import numpy as np
+from itertools import groupby
 
 '''
 ################################################
@@ -57,9 +58,11 @@ They surely help you to augment your source data like a monkey magic! :)
 # Step / Which type of augmentation do you want to apply? (Important! Will be repeatedly used below) 
 name_augmentation = "rotated" # Augmentation type # TODO;
 
+
+
 # Path for images, and annotations 
-path_source = "../../../Media/v0/valid/per_case/blade/" # Path where source images located # TODO;
-path_dest = "../../../Media/v0/valid/per_case/" + name_augmentation + "/" # Path where augmented images located # TODO;
+path_source = "../../../Media/v0.1/valid/per_case/blade/" # Path where source images located # TODO;
+path_dest = "../../../Media/v0.1/valid/per_case/" + name_augmentation + "/" # Path where augmented images located # TODO;
 os.makedirs(path_dest)
 
 path_source_images = path_source + "images" 
@@ -69,6 +72,8 @@ path_dest_images = path_dest + "images"
 path_dest_annotations = path_dest + "annotations" 
 os.makedirs(path_dest_images)
 os.makedirs(path_dest_annotations)
+
+
 
 # Step / Open the original json annotations file
 old_ann_filename = 'coco_annotations.json' # TODO;
@@ -81,6 +86,7 @@ print(dict_keys)
 js_dicts_images = json_data['images']
 js_dicts_annotations = json_data['annotations']
 print("number of original annotations : ", len(js_dicts_annotations))
+
 
 
 # Step / We want to create a new annotations file extracted from the original annotations file
@@ -99,12 +105,9 @@ list_images = os.listdir(path_source_images)
 
 
 # Construct an augmentation pipeline constructed
-height, width = 300, 300 # TODO;
+height, width = 512, 512 # TODO;
 
 transform = A.Compose([ # TODO;
-    A.Resize(height=height, width=width),
-    # A.ShiftScaleRotate(shift_limit=0, scale_limit=0, rotate_limit=15, p=1),
-    # A.HorizontalFlip(p=1)
     A.Rotate(limit=[-15, 15], p=1)
     ],
     bbox_params = A.BboxParams(format='coco', min_visibility=0, label_fields=['category_ids']),
@@ -155,60 +158,90 @@ def visualize_image_with_bbox(img, bboxes, file_name):
         img = cv2.rectangle(img, (x_min, y_min), (x_max, y_max), (0, 256, 0), 1)
     cv2.imwrite(path_dest + file_name, img)
 
+def binary_mask_to_rle(binary_mask):
+    rle = {'counts': [], 'size': list(binary_mask.shape)}
+    counts = rle.get('counts')
+    for i, (value, elements) in enumerate(groupby(binary_mask.ravel(order='F'))):
+        if i == 0 and value == 1:
+            counts.append(0)
+        counts.append(len(list(elements)))
+    return rle
+
+coco = COCO(path_source_annotations + "/" + old_ann_filename)
 
 
 # Step / Augment and Store new images, massks, annotations into a new directory
 for file_name in list_images:
 
+    # if file_name == '191228_105517_252.jpg': # TODO; Test with your own figure!
+
     image_id = ids_dict_with_file_names[file_name]
 
     # Read original data before augmentation
     image = cv2.imread(path_source_images + '/' + file_name)
+
     bboxes = bboxes_dict_with_key_id[image_id]
     category_ids = category_ids_dict_with_key_id[image_id]
 
-    # Create new objects that are augmented
-    augmentations = transform(image=image, bboxes=bboxes, category_ids=category_ids)
-    augmentation_img = augmentations["image"]
-    augmentation_bboxes = augmentations["bboxes"]
+    ann_ids = coco.getAnnIds(imgIds=image_id)
+    anns = coco.loadAnns(ann_ids) 
 
-    # Write new data after augmentation
-    cv2.imwrite(path_dest_images + "/" + file_name, augmentation_img) # TODO;
+    augmentation_zip = {} # Key : ann_id, Value : [cat_id, segmentation] 
+
+    # Create new objects that are augmented
+    for ann in anns:
+        mask = coco.annToMask(ann)
+        cat_id = ann["category_id"]
+        ann_id = ann["id"]
+
+        augmentations = transform(image=image, bboxes=bboxes, mask=mask, category_ids=category_ids) # WARN : Mask should be ndarray !!!
+        augmentation_img = augmentations["image"] # BUG : Image is only one
+        augmentation_mask = augmentations["mask"] # Array filled with 0, 1, 2, 3
+
+        fortran_ground_truth_binary_mask = np.asfortranarray(augmentation_mask) # Problem
+        encoded_ground_truth = pm.encode(fortran_ground_truth_binary_mask) # RLE Object?
+        area = float(pm.area(encoded_ground_truth))
+        bbox = list(pm.toBbox(encoded_ground_truth))
+
+        segmentation = binary_mask_to_rle(fortran_ground_truth_binary_mask)
+        # print(segmentation)
+
+        augmentation_zip[ann_id] = [cat_id, area, bbox, segmentation]
+
 
     # Append new "images" dictionary info; The otherse are maintained inside
     img_dict = {
         "file_name": file_name,
-        "height": 300,
-        "width": 300,
+        "height": 512,
+        "width": 512,
         "id": image_id
     }
     new_img_info.append(img_dict)
     img_id += 1
 
     # Append new "annotations" dictionary info; The otherse are maintained inside
-    for bbox, cat_id in zip(augmentation_bboxes, category_ids):
+    for ann in anns:
+
+        ann_id = ann["id"]
+        aug_zip = augmentation_zip[ann_id]
+        # contour = np.flip(contour, axis=1)
+        # segmentation = contour.ravel().tolist()
+
         annot_dict = {
-            "bbox" : bbox, # Changed;
-            "category_id": cat_id,
+            "bbox" : aug_zip[2], # Changed;
+            "category_id": aug_zip[0],
             "id": annot_id, # Increased;
             "iscrowd": 0, # Fixed;
-            "area": 0, #js_dicts_annotations[i]['area'],
+            "area": aug_zip[1], #js_dicts_annotations[i]['area'],
             "image_id": image_id,
-            "segmentation": [], #js_dicts_annotations[i]['segmentation']
+            "segmentation": [aug_zip[3]]
         }
         new_annot_info.append(annot_dict)
         annot_id += 1 
 
-    # if file_name == '191228_112311_507.jpg': # '191228_102857_995.jpg':
-    #     print("original bboxes : ", bboxes)
-    #     print("original categories : ", category_ids)
-    #     visualize_image_with_bbox(image, bboxes, 'original.jpg')
+    # Write new data after augmentation
+    cv2.imwrite(path_dest_images + "/" + file_name, augmentation_img) # TODO;
 
-    #     print("augmented bbox : ", augmentation_bboxes)
-    #     visualize_image_with_bbox(augmentation_img, augmentation_bboxes, 'augmented.jpg')
-    
-    # if len(bboxes) != len(augmentation_bboxes):
-    #     print(file_name, " at id ", image_id)
 
 # print("annot_info[0] : ", annot_info[0])
 js_dicts_new['images'] = new_img_info
